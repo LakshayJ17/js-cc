7482b13537280e094ab99db479aee64f0e0eb8d8
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-12 22:55:02 +0530

    Add initial JavaScript files for account management and data types

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 1323
+let accountEmail = "jadldakj@gmail.com"
+var accountPassword = "1234"
+accountCity = "Fbd"
+
+
+let accountState
+
+// accountId = 2 // gives error 
+console.log(accountId)
+
+accountEmail = "abc@abc.com"
+accountPassword = "712691827"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 02_datatypes/main.js
@@ -0,0 +1,9 @@
+"use strict"; // teat all JS code as newer version
+
+// alert(3 + 3) 
+
+
+let name = "lakshay"
+let age = 20
+let isLoggedIn = false
+
