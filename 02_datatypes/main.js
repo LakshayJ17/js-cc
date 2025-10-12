0635df426f218e0378e20aacff6fe1d334e2c95e
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-12 22:58:09 +0530

    Refactor comments and console logs for clarity on data types

--- 02_datatypes/main.js
+++ 02_datatypes/main.js
@@ -7,3 +7,13 @@ let name = "lakshay"
 let age = 20
 let isLoggedIn = false
 
+// string = ""
+// boolean = true/ false
+// null = standalone value 
+// undefined 
+// symbol = unqiue
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
