f5ec53cc39da64b2cdf0cef4811562dde30aa778
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-13 23:20:45 +0530

    Add JavaScript file for data type conversions and logging

---
+++ 02_datatypes/02.js
@@ -0,0 +1,28 @@
+let score = "33"
+console.log(score)
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33qbc"=> NaN
+// true => 1
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// "" => false
+// "lakshay" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+
+console.log(typeof StringNumber);
+
