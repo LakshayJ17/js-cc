33e944bc481b3b46591034c27c44682805861b94
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-16 23:44:57 +0530

    summary

---
+++ 02_datatypes/summary.js
@@ -0,0 +1,23 @@
+// Datatypes - primitive and non primitive
+
+// Primitive - 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 10
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const temp = null
+let userEmail
+
+const id = Symbol('112')
+const anotherId = Symbol('112')
+
+console.log(id)
+console.log(anotherId)
+console.log(id == anotherId)
+console.log(id === anotherId)
+
+
+// Reference / non primitive - Array, Objects, Functions
+
+
