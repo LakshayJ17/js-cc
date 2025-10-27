2e1eb6ad78567086529114420823f2ee06dd2fb0
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-27 23:44:14 +0530

    refactor: enhance loginUserMessage function and add object handling functions

--- 03_functions/01_basics.js
+++ 03_functions/01_basics.js
@@ -15,7 +15,32 @@ console.log(addTwoNums(2, 3));
 console.log(addTwoNums(2, "w"));
 
 
-function loginUserMessage(username = "BAC"){
+function loginUserMessage(username = "BAC") {
     return `${username} Welcome!`
 }
 console.log(loginUserMessage())
+
+function CalculateCarPrice(...num1) {
+    return num1
+}
+console.log(CalculateCarPrice(200, 400, 500));
+
+const user = {
+    username: "Lakshay",
+    email: "random@gmail.com",
+    price: 200
+}
+
+function handleObj(anyObj) {
+    console.log(`Username is ${anyObj.username} and email is ${anyObj.email}`)
+}
+
+handleObj(user)
+
+const newArray = [100, 29, 8282, 3829]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(newArray))
