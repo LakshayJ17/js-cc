24a418082aae1e736dc63f95ce30672c957e8b84
lakshay17 <lakshayjain01lj@gmail.com>
2025-10-29 23:10:05 +0530

    hoisitng and scope

--- 03_functions/02_scope.js
+++ 03_functions/02_scope.js
@@ -10,9 +10,48 @@ if (true) {
     let a = 10
     const b = 20
 
-    console.log("INNER : ", a)
+    // console.log("INNER : ", a)
 }
 
-console.log(a); //error if let a is not there in global scope, if let a = 200 in global scope then ans = 200
+// console.log(a); //error if let a is not there in global scope, if let a = 200 in global scope then ans = 200
 // console.log(b); //error
-console.log(c); // undefined
+// console.log(c); // undefined
+
+
+//  child can get parents value but parent cant of children
+function one(){
+    const username = "Lakshay"
+    
+    function two(){
+        const username = "Lakshay Jain"
+        const website = "Chaicode"
+
+        console.log("INSIDE INNER FUNCTION : ", username); // Lakshay Jain
+        console.log("INSIDE INNER FUNCTION : ", website); //chaicode
+        
+    }
+    two()
+    console.log("AFTER INNER FUNCTION : ", username); // Lakshay
+    // console.log("AFTER INNER FUNCTION : ", website); -> this gives error 
+    
+}
+
+one()
+
+//  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function addOne(num){
+    return num + 1
+}
+
+console.log(addOne(5));
+
+// addTwo(6) ReferenceError: Cannot access 'addTwo' before initialization
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(addOne(5)))
+
+
